30ff9b1774cb5aef5f7f9133903be8e5bafee2cc
roman khan <romankhan.26@outlook.com>
2024-07-03 10:09:43 +0500

    first commit

---
+++ digitalOutput.ts
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import ansiEscapes from 'ansi-escapes';
+
+// ASCII art for digits 0-9 in digital clock style
+export const digits: { [key: string]: string[] } = {
+    "0": [
+        " _ ",
+        "| |",
+        "|_|"
+    ],
+    "1": [
+        "   ",
+        "  |",
+        "  |"
+    ],
+    "2": [
+        " _ ",
+        " _|",
+        "|_ "
+    ],
+    "3": [
+        " _ ",
+        " _|",
+        " _|"
+    ],
+    "4": [
+        "   ",
+        "|_|",
+        "  |"
+    ],
+    "5": [
+        " _ ",
+        "|_ ",
+        " _|"
+    ],
+    "6": [
+        " _ ",
+        "|_ ",
+        "|_|"
+    ],
+    "7": [
+        " _ ",
+        "  |",
+        "  |"
+    ],
+    "8": [
+        " _ ",
+        "|_|",
+        "|_|"
+    ],
+    "9": [
+        " _ ",
+        "|_|",
+        " _|"
+    ]
+};
+
+// ASCII art for colon
+export const colon: string[] = [
+    "   ",
+    " . ",
+    " . "
+];
+
+// Function to render each digit in the digital clock style
+export function renderDigit(digit: string, index: number) {
+    const lines = digits[digit];
+    for (let i = 0; i < lines.length; i++) {
+        process.stdout.write(`${ansiEscapes.cursorTo(index * 4, i)}${chalk.cyan.bold(lines[i])}`);
+    }
+}
+
+// Function to render the time in HH:MM:SS format using ASCII art
+export function renderTime(hours: number, minutes: number, seconds: number) {
+    const timeStr = `${hours.toString().padStart(2,'0')}:${minutes.toString().padStart(2,'0')}:${seconds.toString().padStart(2,'0')}`;
+    process.stdout.write(ansiEscapes.clearScreen);  // Clear the console
+    for (let i = 0; i < 3; i++) {  // Three lines for each digit
+        let line = '';
+        for (let j = 0; j < timeStr.length; j++) {
+            if (timeStr[j] === ':') {
+                line += `${chalk.cyan.bold(colon[i])} `;
+            } else {
+                line += `${chalk.cyan.bold(digits[timeStr[j]][i])} `;
+            }
+        }
+        process.stdout.write(`${line}\n`);
+    }
+}
---
+++ main.ts
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+import { differenceInSeconds } from "date-fns"
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { renderTime } from './digitalOutput.js';
+
+
+const userInput = await inquirer.prompt([{
+    name: "hours",
+    type: "number",
+    message: "Please enter the amount of hours:",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "\nPlease Enter Valid Number Of Hours\n"
+        }
+        else {
+            return true
+        }
+    }
+},
+{
+    name: "minutes",
+    type: "number",
+    message: "Please enter the amount of minutes:",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "\nPlease Enter Valid Number Of Minutes\n"
+        } else {
+            return true
+        }
+    }
+},
+{
+    name: "seconds",
+    type: "number",
+    message: "Please enter the amount of seconds:",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "\nPlease Enter Valid Number Of Seconds\n"
+        } else {
+            return true
+        }
+    }
+}
+])
+
+
+
+async function countdownTimer(hours: number, minutes: number, seconds: number) {
+    const totaltime= (hours *3600) + (minutes*60) + seconds
+    const initialTime = new Date().setSeconds(new Date().getSeconds() + totaltime)
+    const time = new Date(initialTime)
+    setInterval((() => {
+        const currentTime = new Date()
+        const timeDiff = differenceInSeconds(time, currentTime)
+        if (timeDiff <= 0) {
+            console.log(chalk.cyan.bold(`\n<< Countdown Finished >>\n`));
+            console.log(chalk.cyan.bold(`\n\n<-------------------------------------------------------------------------------------------------->\n\n`));
+            process.exit()
+        }
+
+        const hr = Math.floor((timeDiff % (3600 * 24)) / 3600)
+        const min = Math.floor((timeDiff % 3600) / 60)
+        const sec = Math.floor(timeDiff % 60)
+         //console.log(`${hr.toString().padStart(2, "0")}h : ${min.toString().padStart(2, "0")}m : ${sec.toString().padStart(2, "0")}s`);
+        renderTime(hr, min, sec)
+
+    }), 1000)
+
+}
+
+countdownTimer(userInput.hours, userInput.minutes, userInput.seconds)
+
